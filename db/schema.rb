1a5f00c6413a496ecf7e9833cfb28ff1f1c953d4
kiat <kiat.mao@gmail.com>
2020-06-08 10:29:53 +0800

    create business

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_072038) do
+ActiveRecord::Schema.define(version: 2020_06_03_061457) do
+
+  create_table "businesses", force: :cascade do |t|
+    t.string "code", null: false
+    t.string "name", null: false
+    t.string "btype"
+    t.string "industry"
+    t.integer "time_limit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.integer "user_id"
