ff4545c8bb1942f09954b21bd248787dddddd784
kiat <kiat.mao@gmail.com>
2021-06-18 15:58:03 +0800

    add a validates on business

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,6 @@
 class Business < ApplicationRecord
-	validates_presence_of :code, :name, :is_init_expresses_midday, :is_all_visible, :message => '不能为空'
+	validates_presence_of :code, :name, :message => '不能为空'
+	validates_inclusion_of :is_init_expresses_midday, :is_all_visible, in: [true, false], :message => '只能是或否'
 	validates_uniqueness_of :code, :message => '该客户代号已存在'
 
 	def self.select_btypes(is_court)
