2bd2fd5d324a7b7e4d49bcc76bcb162903101200
yyy <kiat.mao@gmail.com>
2023-04-10 11:06:15 +0800

    create tables for international expresses

--- db/migrate/20230410013951_create_time_limits.rb
+++ db/migrate/20230410013951_create_time_limits.rb
@@ -1,7 +1,7 @@
 class CreateTimeLimits < ActiveRecord::Migration[6.0]
   def change
     create_table :time_limits do |t|
-    	t.string   :country
+    	t.string   :country, :null => false, :unique => true
     	t.integer  :interchangeA1
     	t.integer  :interchangeA2
     	t.integer  :airB
--- db/migrate/20230410015032_add_is_international_to_businesses.rb
+++ db/migrate/20230410015032_add_is_international_to_businesses.rb
@@ -1,5 +1,5 @@
 class AddIsInternationalToBusinesses < ActiveRecord::Migration[6.0]
   def change
-  	add_column :businesses, :is_international, :boolean
+  	add_column :businesses, :is_international, :boolean, default: false 
   end
 end
