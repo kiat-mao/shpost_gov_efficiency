ffcd63064551cf2daeb0653f5ae0c29ccd189bb0
yyy <kiat.mao@gmail.com>
2023-12-18 10:28:24 +0800

    update mail_trace_his.rb

--- app/models/mail_trace_his.rb
+++ app/models/mail_trace_his.rb
@@ -5,7 +5,7 @@ class MailTraceHis < PkpDataRecordHis
   def traces
     jdpt_traces = self.jdpt_traces
     if !jdpt_traces.blank?
-      jdpt_traces.sort{|x,y| x['opTime'] <=> y['opTime']}.map{|x| "#{x['opTime']}  #{x['opDesc']}, 操作代码: #{x['opCode']}, 操作名称: #{x['opDesc']}, 操作人: #{x['operatorName']}, 操作单位: #{x['opOrgName']} \n"}.uniq.join
+      jdpt_traces.sort{|x,y| x['opTime'] <=> y['opTime']}.map{|x| "#{x['opTime']}  #{x['opDesc']}, 操作代码: #{x['opCode']}, 操作名称: #{x['opName']}, 操作人: #{x['operatorName']}, 操作单位: #{x['opOrgName']} \n"}.uniq.join
     else
       ""
     end
