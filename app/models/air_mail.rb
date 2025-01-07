00341d9b94d291d03f6eb73c05b140e8ff978535
yyy <kiat.mao@gmail.com>
2025-01-07 10:32:10 +0800

    update

--- app/models/air_mail.rb
+++ app/models/air_mail.rb
@@ -1,5 +1,6 @@
 class AirMail < ApplicationRecord
   belongs_to :post_unit, class_name: 'Unit', optional: true
+  has_one :post_parent_unit, class_name: 'Unit', through: :post_unit, source: 'parent_unit'
  
   enum direction: {import: 'import', export: 'export'}
   DIRECTION = {import: '进口', export: '出口'}
